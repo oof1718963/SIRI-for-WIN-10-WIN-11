f8963108d21072e0bdc6359ebefa2445ef61c5c3
oof1718963 <75444840+oof1718963@users.noreply.github.com>
2022-10-08 12:17:35 -0400

    Update Translator.py

--- Translator.py
+++ Translator.py
@@ -1,53 +1,45 @@
 from tkinter import *
-from PIL import ImageTk , Image
 from tkinter import ttk
-from googletrans import Translator , LANGUAGES
-from googletrans import Translator
-from googletrans import LANGUAGES
+from googletrans import Translator, LANGUAGES
 
 root = Tk()
-root.geometry("1000x1000")
-root.title("Translator")
-root.config(bg="white")
-
-title_label = Label(root,text="Translator",bg="white")
-title_label.place(relx=0.5,rely=0.1,anchor=CENTER)
-
-label1 = Label(root,text="Enter Text")
-label1.place(relx=0.2,rely=0.3,anchor=W)
-textarea1 = Text(root,width=30,height=10)
-textarea1.place(relx=0.1,rely=0.5,anchor=W)
-root.mainloop()
+root.geometry("1080x400")
+root.configure(bg="#F2CCC3")
 root.title("Language Translator")
-root.geometry("600x400")
-root.configure(bg = "#fced95")
-language = list(LANGUAGES.values())
 
+language=list(Languages.values())
+lb_title=Label(root, text="LANGUAGE TRANSLATOR", bg="#F2CCC3")
+lb_title.place(relx=0.5,rely=0.1,anchor=CENTER)
 
-label_heading = Label(root, text = "Language Translator", bg = "#fced95", font = ("SF Pro Display", 30, "bold"))
-label_heading.place(relx = 0.5, rely = 0.1, anchor = CENTER)
+label_input=Label(root, text="Enter Text", bg="#F2CCC3", font="arial 13 bold"
+label_input.place(relx=0.02,rely=0.2,anchor=W)
+src_lang=ttk.Combobox(root, values=language, width=23, state="readonly")
+src_lang.place(relx=0.13,rely=0.2,anchor=W)
+src_lang.set("english")
 
-input_label = Label(root, text = "Enter Text", bg = "#fced95", font = ("SF Pro Display", 17 , "bold"))
-input_label.place(relx = 0.02, rely = 0.3, anchor = W)
+lb_output=Label(root, text="Enter Text", bg="#F2CCC3", font="arial 13 bold"
+lb_output.place(relx=0.81,rely=0.2,anchor=E)
+dest_lang=ttk.Combobox(root, values=language, width=22, state="readonly")
+dest_lang.place(relx=0.98,rely=0.2,anchor=E)
+dest_lang.set("choose output language")
 
-d1 = ttk.Combobox(root, state = "readonly", values = language, width = 25)
-d1.place(relx = 0.02, rely = 0.2, anchor = W)
-d1.set("English")
+Input_text=Text(root,font="arial 10",height=11,wrap=WORD,padx=5,pady=5)
+Input_text.place(relx=0.02,rely=0.5,anchor=W)
+Output_text=Text(root,font="arial 10",height=11,wrap=WORD,padx=5,pady=5)
+Output_text.place(relx=0.98,rely=0.5,anchor=E)
 
-input_text = Text(root, bg = "#f2b8da", font = ("SF Pro Display", 15, "bold"), height = 6, wrap = WORD, width = 20, padx = 10, pady = 1, bd = 0)
-input_text.place(relx = 0.02, rely = 0.55, anchor = W)
-
-output_label = Label(root, text = "Output", bg = "#fced95", font = ("SF Pro Display", 20, "bold"))
-output_label.place(relx = 0.98, rely = 0.3, anchor = E)
-
-d2 = ttk.Combobox(root, state = "readonly", values = language, width = 25)
-d2.place(relx = 0.98, rely = 0.2, anchor = E)
-d2.set("Choose Output Language")
-
-output_text = Text(root, bg = "#f2b8da", font = ("SF Pro Display", 15, "bold"), height = 6, wrap = WORD, width = 20, padx = 10, pady = 1, bd = 0)
-output_text.place(relx = 0.98, rely = 0.55, anchor = E)
-
-translate_btn = Button(root, text = "Translate", font = ("Bell MT", 15, "bold"), bg = "#f2b8da", fg = "black", relief = FLAT, padx = 10, pady = 1)
-translate_btn.place(relx = 0.5, rely = 0.8, anchor = CENTER)
-
+def Translate():
+    translator=Translator()
+    try:
+        translated=translator.translate(text=Input_text.get(1.0,END), src=src)
+        Output_text.delete(1.0,END)
+        Output_text.insert(END, translated.text)
+    except:
+        print("try again")
+        
+trans_btn=Button(root,text="Translate",font="arial 12 bold",pady=5,command=Translate)
+trans_btn.place(relx=0.5,rely=0.85,anchor=CENTER)
+lb_footer=Label(root, text="Created By Whitehat Jr Team", bg="#F2CCC3", font="arial 13 bold"
+lb_footer.place(relx=0.81,rely=0.2,anchor=CENTER))
+lb_footer.place(relx=0.5,rely=0.97,anchor=CENTER)
 root.mainloop()
